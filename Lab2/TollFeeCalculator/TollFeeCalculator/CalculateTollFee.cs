e397c3cf6d6364b8798bed4ab7b065e52aa1d19d
Daniel <daniel.andersson@iths.se>
2020-11-30 16:05:32 +0100

    Started with unittests

--- Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
@@ -2,58 +2,80 @@
 
 namespace TollFeeCalculator
 {
-    class Program
-    {
-        static void Main()
-        {
-            run(Environment.CurrentDirectory + "../../../../testData.txt");
-        }
+	public class Program
+	{
+		static void Main()
+		{
+			run(Environment.CurrentDirectory + "../../../../testData.txt");
+		}
 
-        static void run(String inputFile) {
-            string indata = System.IO.File.ReadAllText(inputFile);
-            String[] dateStrings = indata.Split(", ");
-            DateTime[] dates = new DateTime[dateStrings.Length-1];
-            for(int i = 0; i < dates.Length; i++) {
-                dates[i] = DateTime.Parse(dateStrings[i]);
-            }
-            Console.Write("The total fee for the inputfile is" + TotalFeeCost(dates));
-        }
+		public static void run(String inputFile)
+		{
+			string indata;
+			try
+			{
+				indata = System.IO.File.ReadAllText(inputFile);
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+			Program program = new Program();
+			var dates = program.GetDatesFromFile(inputFile);
+			Console.Write("The total fee for the inputfile is" + TotalFeeCost(dates));
+		}
 
-        static int TotalFeeCost(DateTime[] d) {
-            int fee = 0;
-            DateTime si = d[0]; //Starting interval
-            foreach (var d2 in d)
-            {
-                long diffInMinutes = (d2 - si).Minutes;
-                if(diffInMinutes > 60) {
-                    fee += TollFeePass(d2);
-                    si = d2;
-                } else {
-                    fee += Math.Max(TollFeePass(d2), TollFeePass(si));
-                }
-            }
-            return Math.Max(fee, 60);
-        }
+		public DateTime[] GetDatesFromFile(string input)
+		{
+			String[] dateStrings = input.Split(", ");
+			DateTime[] dates = new DateTime[dateStrings.Length];
+			for (int i = 0; i < dates.Length; i++)
+			{
+				dates[i] = DateTime.Parse(dateStrings[i]);
+			}
+			return dates;
+		}
 
-        static int TollFeePass(DateTime d)
-        {
-            if (free(d)) return 0;
-            int hour = d.Hour;
-            int minute = d.Minute;
-            if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-            else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-            else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-            else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-            else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-            else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-            else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-            else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-            else return 0;
-        }
-        //Gets free dates
-        static bool free(DateTime day) {
-        return (int)day.DayOfWeek == 5 || (int)day.DayOfWeek == 6 || day.Month == 7;
-        }
-    }
+		static int TotalFeeCost(DateTime[] d)
+		{
+			int fee = 0;
+			DateTime si = d[0]; //Starting interval
+			foreach (var d2 in d)
+			{
+				long diffInMinutes = (d2 - si).Minutes;
+				if (diffInMinutes > 60)
+				{
+					fee += TollFeePass(d2);
+					si = d2;
+				}
+				else
+				{
+					fee += Math.Max(TollFeePass(d2), TollFeePass(si));
+				}
+			}
+			return Math.Max(fee, 60);
+		}
+
+		static int TollFeePass(DateTime d)
+		{
+			if (free(d)) return 0;
+			int hour = d.Hour;
+			int minute = d.Minute;
+			if (hour == 6 && minute >= 0 && minute <= 29) return 8;
+			else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
+			else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
+			else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
+			else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+			else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
+			else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
+			else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
+			else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+			else return 0;
+		}
+		//Gets free dates
+		static bool free(DateTime day)
+		{
+			return (int)day.DayOfWeek == 5 || (int)day.DayOfWeek == 6 || day.Month == 7;
+		}
+	}
 }
---
+++ Lab2/TollFeeCalculator/UnitTestProject1/UnitTest.cs
@@ -0,0 +1,123 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TollFeeCalculator;
+
+namespace TollFeeCalculatorTest
+{
+	[TestClass]
+	public class UnitTest
+	{
+
+		//13: string indata = System.IO.File.ReadAllText(inputFile); // felhantering
+		[TestMethod]
+		public void ReadFromDirectoryTest()
+		{
+			//Arrange
+			string wrongDirectory = "adfasfasfaf.txt";
+			string emptyString = "";
+			//Act
+			//Assert
+			Assert.ThrowsException<FileNotFoundException>(() => Program.run(wrongDirectory));
+			Assert.ThrowsException<ArgumentException>(() => Program.run(emptyString));
+		}
+
+		//DateTime[] dates = new DateTime[dateStrings.Length - 1]; // tar bort sista datumet
+		[TestMethod]
+		public void GetDatesFromFileTest()
+		{
+			//Arrange
+			Program program = new Program();
+			var testString = "2020-06-30 00:05, 2020-06-30 06:34";
+			var expected = new DateTime[] {
+				DateTime.Parse("2020-06-30 00:05"),
+				DateTime.Parse("2020-06-30 06:34")
+			};
+			var wrongTextToDateTimeParse = "654166";
+			//Act
+			var actual = program.GetDatesFromFile(testString);
+			//Assert
+			CollectionAssert.AreEqual(expected, actual);
+			Assert.ThrowsException<FormatException>(() => program.GetDatesFromFile(testString));
+			Assert.ThrowsException<NullReferenceException>(() => program.GetDatesFromFile(null));
+		}
+
+		//dates[i] = DateTime.Parse(dateStrings[i]); // felhantering
+		[TestMethod]
+		public void TestMethod3()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+
+		//Console.Write("The total fee for the inputfile is" + TotalFeeCost(dates)); // sakna mellanslag
+		[TestMethod]
+		public void TestMethod4()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+
+		//long diffInMinutes = (d2 - si).Minutes; // räknar bara minuter inte timmar returnar alltid -59 - 59 så aldrig över 60 TotalMinutes
+		[TestMethod]
+		public void TestMethod5()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+
+		//fee += Math.Max(TollFeePass(d2), TollFeePass(si)); // lägga till skillande inte båda värdena
+		[TestMethod]
+		public void TestMethod6()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+
+		//return Math.Max(fee, 60); // retunerar alltid 60 borde vara Min istället+
+		[TestMethod]
+		public void TestMethod7()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+
+		//else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8; // andra delen av statmentet är fel
+		//else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18; // skall vara får 1530 inte 1500 fel grejer som är fuckade
+		[TestMethod]
+		public void TestMethod8()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+
+		//return (int)day.DayOfWeek == 5 || (int)day.DayOfWeek == 6 || day.Month == 7; // söndag är 0 och lördag är 6
+		[TestMethod]
+		public void TestMethod9()
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+		}
+	}
+}
